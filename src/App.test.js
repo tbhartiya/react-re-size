8b789dd525a12906bd85de830d03947b38d82692
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2022-07-04 09:00:33 +0530

    add testing

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("renders text sidebar", () => {
+  render(<App />);
+  const text = screen.queryByText("sidebar");
+  expect(text).toBeTruthy();
+});
